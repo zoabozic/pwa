1290fd6082ce79d7c2d47fc620676b118d5bcb46
zoabozic <zoabozic@gmail.com>
2026-03-25 13:32:20 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,24 @@
+<?php
+
+$naslov = "prva vjezba";
+$autor = "zoa bozic";
+
+echo "<h1>$naslov</h1>";
+echo "<p>ovu stranicu izradila je $autor.</p>";
+echo '<a href="https://www.youtube.com/">posjeti youtube</a>';
+
+?>
+
+<!-- naziv datoteke: vjezba1.php-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>peva vjezba</title>
+</head>
+<body>
+    
+</body>
+</html>
